fea7fac006c70fd1eaedfe8bd78764635c69b70d
Jeremy Busk <jeremybusk@gmail.com>
2021-09-23 07:48:54 -0600

    style clean-up add flake8

--- ci-main.sh
+++ ci-main.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 set -e
+
+echo "Style check."
+pip install flake8 && flake8 app.py
+
+echo "Build and push docker container to Dockerhub."
+release=latest
+docker build --tag uvoo/monitor-utils:$release .
 echo $DOCKERHUB_TOKEN | docker login --username $DOCKERHUB_USERNAME --password-stdin
-./buildPush.sh
+docker push uvoo/monitor-utils:$release
+docker logout
