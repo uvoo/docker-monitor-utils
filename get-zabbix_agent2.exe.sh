187e1c2cb2bcccc8f7194224857614d629e8e432
Jeremy Busk <jeremybusk@gmail.com>
2022-04-04 12:46:25 -0600

    update to agent v5.4.12

--- get-zabbix_agent2.exe.sh
+++ get-zabbix_agent2.exe.sh
@@ -4,10 +4,12 @@ set -eux
 tmpdir=$(mktemp -d -t tmp-XXX)
 major=5
 minor=4
-patch=8
+patch=12
 release=$major.$minor.$patch
 archive_file=${tmpdir}/zabbix_agent2-${release}-windows-amd64-openssl-static.zip
 curl -L https://cdn.zabbix.com/zabbix/binaries/stable/${major}.${minor}/${release}/zabbix_agent2-${release}-windows-amd64-openssl-static.zip -o $archive_file 
 unzip -oj $archive_file  *zabbix_agent2.exe -d downloads  || true
 sha256sum downloads/zabbix_agent2.exe | awk '{print $1}' > downloads/zabbix_agent2.exe.sha256
+echo "Version: $major.$minor.$patch" > downloads/README.md
+echo "URL: https://cdn.zabbix.com/zabbix/binaries/stable/${major}.${minor}/${release}/zabbix_agent2-${release}-windows-amd64-openssl-static.zip" >> downloads/README.md
 rm -rf $tmpdir
