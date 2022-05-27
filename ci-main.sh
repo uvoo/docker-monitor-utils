31a4d30fa777fc89badcdd73e119ad656ab072f5
Jeremy Busk <jeremybusk@gmail.com>
2022-05-27 08:18:00 -0600

    add sleep before dh push

--- ci-main.sh
+++ ci-main.sh
@@ -7,6 +7,7 @@ pip install flake8 && flake8 app.py
 echo "Build and push docker container to Dockerhub."
 release=latest
 docker build --tag uvoo/monitor-utils:$release .
+echo Push to docker repo in 5 seconds; sleep 5
 echo $DOCKERHUB_TOKEN | docker login --username $DOCKERHUB_USERNAME --password-stdin
 docker push uvoo/monitor-utils:$release
 docker logout
