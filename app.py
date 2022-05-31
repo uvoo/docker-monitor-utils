6b90f90322218a02a07b397bb65676f251c98fbd
Jeremy Busk <jeremybusk@gmail.com>
2022-05-31 12:37:29 -0600

    run as user app

--- app.py
+++ app.py
@@ -436,6 +436,6 @@ def get_root():
 
 
 if __name__ == '__main__':
-    # app.run(debug=False, host='0.0.0.0', port=80)
-    from waitress import serve
-    serve(app, host='0.0.0.0', port=80)
+    app.run(debug=False, host='0.0.0.0', port=8080)
+    # from waitress import serve
+    # serve(app, host='0.0.0.0', port=80)
