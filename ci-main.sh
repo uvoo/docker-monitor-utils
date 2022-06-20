f885e10fc89883ec43584f78e868d833d657d5b3
Jeremy Busk <jeremybusk@gmail.com>
2022-06-20 12:53:59 -0600

    push latest

--- ci-main.sh
+++ ci-main.sh
@@ -10,4 +10,5 @@ docker build --tag uvoo/monitor-utils:latest --tag uvoo/monitor-utils:$release .
 echo Push to docker repo in 5 seconds; sleep 5
 echo $DOCKERHUB_TOKEN | docker login --username $DOCKERHUB_USERNAME --password-stdin
 docker push uvoo/monitor-utils:$release
+docker push uvoo/monitor-utils:latest
 docker logout
