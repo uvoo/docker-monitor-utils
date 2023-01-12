79e28f0f623d4cdee22311307daaeaa2b34984b9
Jeremy Busk <jeremybusk@gmail.com>
2023-01-11 18:34:35 -0700

    update var name

--- ci-main.sh
+++ ci-main.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+set -eu
 
 echo "Style check."
 pip install flake8 && flake8 app.py
@@ -8,7 +8,7 @@ echo "Build and push docker container to Dockerhub."
 release=6.0.12
 docker build --tag uvoo/monitor-utils:latest --tag uvoo/monitor-utils:$release .
 echo Push to docker repo in 5 seconds; sleep 5
-echo $DOCKERHUB_TOKEN | docker login --username $DOCKERHUB_USERNAME --password-stdin
+echo $DOCKERHUB_USERTOKEN | docker login --username $DOCKERHUB_USERNAME --password-stdin
 docker push uvoo/monitor-utils:$release
 docker push uvoo/monitor-utils:latest
 docker logout
