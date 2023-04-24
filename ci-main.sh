1160aa2816b7e78683055b1badd966c207dff8a5
Jeremy Busk <jeremybusk@gmail.com>
2023-04-24 08:57:33 -0600

    change dh pname

--- ci-main.sh
+++ ci-main.sh
@@ -8,7 +8,7 @@ echo "Build and push docker container to Dockerhub."
 release=6.0.17
 docker build --tag uvoo/monitor-utils:latest --tag uvoo/monitor-utils:$release .
 echo Push to docker repo in 5 seconds; sleep 5
-echo $DOCKERHUB_USERTOKEN | docker login --username $DOCKERHUB_USERNAME --password-stdin
+echo $DOCKERHUB_PASSWORD | docker login --username $DOCKERHUB_USERNAME --password-stdin
 docker push uvoo/monitor-utils:$release
 docker push uvoo/monitor-utils:latest
 docker logout
